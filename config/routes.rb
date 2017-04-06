4e97f7a0c18741bbad4c6a5f8b9336669a7f14f6
Ricardo <ricardo_emerson@yahoo.com.br>
2017-04-05 22:40:49 -0500

    Add into routes.rb the information to use subdomain in development environment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Setup subdomain for rails applications in /etc/hosts.
+  # 127.0.0.1 api.task-manager.dev
+
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
   end
